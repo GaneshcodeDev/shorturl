38a2ced380308bcc45f31f15c2ce791a13cc428b
Ganesh Pandey <pandeyganesh999@gmail.com>
2025-03-26 18:51:44 +0530

    intial 2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -22,6 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
+        Ganesh Pandey
           Edit <code>src/App.jsx</code> and save to test HMR ( Ganesh Pandey )
         </p>
       </div>
